2638bf1467fb993497777708bceec24639b8b3df
Antonio Ortiz <antonioortizdev@gmail.com>
2022-12-19 20:53:44 +0100

    refactor: invert if

--- src/getCategoryPath.js
+++ src/getCategoryPath.js
@@ -4,14 +4,15 @@ const getCategoryPath = (categories, categoryName) => {
       if (category.name === targetName) {
         return category;
       }
-      if (category.subcategories) {
-        for (const subcategory of category.subcategories) {
-          subcategory.parent = category;
-        }
-        const found = traverseCategories(category.subcategories, targetName);
-        if (found) {
-          return found;
-        }
+      if (!category.subcategories) {
+        continue;
+      }
+      for (const subcategory of category.subcategories) {
+        subcategory.parent = category;
+      }
+      const found = traverseCategories(category.subcategories, targetName);
+      if (found) {
+        return found;
       }
     }
     return null;
