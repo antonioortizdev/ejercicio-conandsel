131a48afd533c3e538f6786a3e8354d9a0dcccdb
Antonio Ortiz <antonioortizdev@gmail.com>
2022-12-19 19:59:15 +0100

    fix tests definition

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,11 @@
-test("'category4' should output: '/category1/category3/category4'", () => {
-  expect('/category1/category3/category4').toBe(getCategoryPath(categories, "category4"))
-})
-test("'category2' should output: '/category1/category2'", () => {
-  expect('/category1/category2').toBe(getCategoryPath(categories, "category2"))
-})
-test("'category5' should output: '/category5'", () => {
-  expect('/category5').toBe(getCategoryPath(categories, "category5"))
-})
+test("passing 'category4' should output: '/category1/category3/category4'", () => {
+  expect(getCategoryPath(categories, "category4")).toBe("/category1/category3/category4")
+});
+
+test("passing 'category2' should output: '/category1/category2'", () => {
+  expect(getCategoryPath(categories, "category2")).toBe("/category1/category2")
+});
+
+test("passing 'category5' should output: '/category5'", () => {
+  expect(getCategoryPath(categories, "category5")).toBe("/category5")
+});
