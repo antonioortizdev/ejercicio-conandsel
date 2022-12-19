6cc57b38e4ea7b6d13fb3173f699fa90c6ce9f0e
Antonio Ortiz <antonioortizdev@gmail.com>
2022-12-19 20:10:15 +0100

    implement function to pass all tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,3 +1,61 @@
+const categories = [
+  {
+    name: "category1",
+    subcategories: [
+      {
+        name: "category2",
+        subcategories: [],
+      },
+      {
+        name: "category3",
+        subcategories: [
+          {
+            name: "category4",
+            subcategories: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "category5",
+    subcategories: [],
+  },
+];
+
+// TO-DO: Implement this function
+
+const getCategoryPath = (categories, categoryName) => {
+  const traverseCategories = (categories, targetName) => {
+    for (const category of categories) {
+      if (category.name === targetName) {
+        return category;
+      }
+      for (const subcategory of category.subcategories) {
+        subcategory.parent = category;
+      }
+      const found = traverseCategories(category.subcategories, targetName);
+      if (found) {
+        return found;
+      }
+    }
+    return null;
+  }
+
+  const category = traverseCategories(categories, categoryName);
+  if (!category) {
+    return null;
+  }
+  let path = category.name;
+  let parent = category.parent;
+  while (parent) {
+    path = `${parent.name}/${path}`;
+    parent = parent.parent;
+  }
+  return `/${path}`;
+};
+
+
 test("passing 'category4' should output: '/category1/category3/category4'", () => {
   expect(getCategoryPath(categories, "category4")).toBe("/category1/category3/category4")
 });
