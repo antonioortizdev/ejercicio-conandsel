25075d6e81b3447ba8e65eb2ba49e2eec6cf756d
Antonio Ortiz <antonioortizdev@gmail.com>
2022-12-19 19:49:23 +0100

    define tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,9 @@
-const sum = require('../src/main.js');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+test("'category4' should output: '/category1/category3/category4'", () => {
+  expect('/category1/category3/category4').toBe(getCategoryPath(categories, "category4"))
+})
+test("'category2' should output: '/category1/category2'", () => {
+  expect('/category1/category2').toBe(getCategoryPath(categories, "category2"))
+})
+test("'category5' should output: '/category5'", () => {
+  expect('/category5').toBe(getCategoryPath(categories, "category5"))
+})
